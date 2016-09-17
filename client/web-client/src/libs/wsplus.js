202573d15446d1372047bf0936ecda303017f3e6
tsenterd <tsenterd@gmail.com>
2016-09-17 19:01:24 -0400

    first working copy of web client

---
+++ client/web-client/src/libs/wsplus.js
@@ -0,0 +1,30 @@
+// import * as convert from "./arrayBufHelpers";
+export default function WebSocketPlus(server) {
+  const ws = new WebSocket(server);
+  this.onOpen = () => {};
+  this.onClose = () => {};
+  this.onMessage = () => {};
+  this.sendArrayBuffer = ab => ws.send(ab);
+  this.sendJSON = json => {
+    ws.send(JSON.stringify(json));
+    console.log("Sending", JSON.stringify(json));
+  }
+  this.sendBuffer = data => {
+    ws.send(data)
+    console.log("Sending Buffer")
+
+  }
+  ws.onopen = () => {
+    console.log("Connection Established");
+    this.onOpen();
+  }
+  ws.onclose = () => {
+    console.log("Connection Terminated");
+    this.onClose();
+  }
+  ws.onmessage = (evt) => {
+    console.log("Recieved Message:", evt.data);
+    this.onMessage(evt);
+  }
+
+}
